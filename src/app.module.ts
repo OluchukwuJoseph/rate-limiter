432abccaa794880f58a49d64cc2b465b67ee6f1b
Joseph Oluchukwu <devjoseph016@gmail.com>
2025-07-29 13:10:07 +0100

    feat: implement authentication module with signup and signin functionality

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,12 +7,16 @@ import { DatabaseModule } from './database/database.module';
 import { AppLoggerModule } from './shared/logger/app-logger.module';
 import { RateLimiterMiddleware } from './shared/middleware/rate-limiter.middleware';
 import { HttpLoggerMiddleware } from './shared/middleware/http-logger.middleware';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     DatabaseModule,
     AppLoggerModule,
+    UserModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
